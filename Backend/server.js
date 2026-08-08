32988fd5e92f2e0a3ad79c8f3c1bb7fff7d2d65a
Ishwari Daphal <ishwaridaphal4@gmail.com>
2026-08-08 23:54:40 +0530

    Authentication improved

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,11 @@ import authroutes from "./routes/auth.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use("/api/auth", authroutes);
 app.use("/api", chatroutes);
